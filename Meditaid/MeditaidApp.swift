55bcdf4db0c9daa43127ff848c2185fdfb1f1084
Krzysztof Garmulewicz <garmulewicz@me.com>
2025-12-01 15:28:46 +0100

    Initial Commit

---
+++ Meditaid/MeditaidApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditaidApp.swift
+//  Meditaid
+//
+//  Created by Krzysztof Garmulewicz on 01/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct MeditaidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
